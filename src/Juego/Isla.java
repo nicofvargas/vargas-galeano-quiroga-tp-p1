6ef716c17900ac5f4431a63d061aac4abfd54d6e
nicofvargas <nicofvargass@gmail.com>
2024-10-11 16:36:41 -0300

    se ordeno los metodos fuera del constructor Juego

--- src/Juego/Isla.java
+++ src/Juego/Isla.java
@@ -9,13 +9,18 @@ public class Isla {
     private double y;
     private double ancho;
     private double alto;
+
+    // Constructor que establece las posiciones y dimensiones de una isla
     public Isla(double x, double y, double ancho, double alto) {
         this.x = x;
         this.y = y;
         this.ancho = ancho;
         this.alto = alto;
     }
+
+    // Método que dibuja la isla en el entorno
     public void dibujar(Entorno entorno) {
-        entorno.dibujarRectangulo(this.x,this.y,this.ancho,this.alto,0, Color.yellow);
+        entorno.dibujarRectangulo(this.x, this.y, this.ancho, this.alto, 0, Color.yellow);
     }
 }
+
--- src/Juego/Juego.java
+++ src/Juego/Juego.java
@@ -7,8 +7,7 @@ import java.awt.Color;
 import entorno.Entorno;
 import entorno.InterfaceJuego;
 
-public class Juego extends InterfaceJuego
-{
+public class Juego extends InterfaceJuego {
     // El objeto Entorno que controla el tiempo y otros
     private Entorno entorno;
     private Jugador jugador;
@@ -19,52 +18,35 @@ public class Juego extends InterfaceJuego
     // ...
 
 
-    Juego()
+    Juego() //este metodo es solo un constructor
     {
-        // Inicializa el objeto entorno
         this.entorno = new Entorno(this, "Proyecto para TP", 800, 600);
         this.jugador = new Jugador(entorno,gravedad);
         this.gravedad = new Gravedad();
-        // Inicializar lo que haga falta para el juego
-        // Isla
-        double anchoIsla = 150;
-        double altoIsla = 50;
-        double separacion = 25;
-
-        // Definimos la posición de las islas en el eje Y
-        double[] posicionesY = {500,400,300,200,100};
-
-        // Creamos las islas
-        this.islas = new Isla[15]; // Inicializar la variable islas
-        // ...
-        int indice = 0;
-        for (int i = 0; i < posicionesY.length; i++) {
-            for (int j = 0; j < 5 - i; j++) {
-                double x = ((200 + (anchoIsla*i)) / 2 + (j * (anchoIsla + separacion)));
-
-                double y = posicionesY[i];
-                this.islas[indice] = new Isla(x, y, anchoIsla, altoIsla);
-
-                indice++;
-            }
-
-        }
-        this.islas[14] = new Isla((800 - anchoIsla)/ 2, 50, anchoIsla, altoIsla);
-        // ...
-
-
-
-
-        // Inicia el juego!
+        this.islas = new Isla[15];
+        crearIslas();
         this.entorno.iniciar();
     }
 
-    /**
-     * Durante el juego, el método tick() será ejecutado en cada instante y
-     * por lo tanto es el método más importante de esta clase. Aquí se debe
-     * actualizar el estado interno del juego para simular el paso del tiempo
-     * (ver el enunciado del TP para mayor detalle).
-     */
+    private void crearIslas() {
+        double anchoIsla = 150;
+        double altoIsla = 50;
+        double separacion = 25;
+        double[] posicionesY = {500, 400, 300, 200, 100};
+
+        int indice = 0;
+        for (int i = 0; i < posicionesY.length; i++) {
+            for (int j = 0; j < 5 - i; j++) {
+                double x = ((200 + (anchoIsla * i)) / 2 + (j * (anchoIsla + separacion)));
+                double y = posicionesY[i];
+                this.islas[indice] = new Isla(x, y, anchoIsla, altoIsla);
+                indice++;
+            }
+        }
+        this.islas[14] = new Isla((800 - anchoIsla) / 2, 50, anchoIsla, altoIsla); // Última isla
+    }
+
+
     public void tick()
     {
         for (Isla isla : islas) {
