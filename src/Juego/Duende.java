b9ac775829094af91c3fa680e74b9d46c8a9ccb6
nicofvargas <nicofvargass@gmail.com>
2024-10-14 23:51:05 -0300

    agregue atributos y getters

--- src/Juego/Duende.java
+++ src/Juego/Duende.java
@@ -1,4 +1,23 @@
 package Juego;
 
 public class Duende {
+    private double x;
+    private double y;
+    private double ancho;
+    private double alto;
+    private double velocidad;
+
+    public Duende() {
+        this.x = 100;
+        this.y = 50;
+        this.ancho = 50;
+        this.alto = 50;
+        this.velocidad = 2;
+    }
+
+    //getters
+    public double getX() {
+        return this.x;
+    }
+
 }
