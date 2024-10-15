ff6b87ca171e3154d804796652481f39655019aa
martindev3 <qmartinedgardo@gmail.com>
2024-10-15 12:40:43 -0300

    Comento la clase Tortuga, por que no logro hacer que caigan del cielo

--- src/Juego/Tortuga.java
+++ src/Juego/Tortuga.java
@@ -7,45 +7,50 @@ public class Tortuga {
 
     private boolean estaEnIsla;
     private Isla islaActual;
+    private double velocidadCaída;
 
-    public Tortuga() {
-        this.x = 0;
-        this.y = 0;
+    public Tortuga(double x, double y) {
+        this.x = x;
+        this.y = y;
         this.ancho = 10;
         this.alto = 10;
         estaEnIsla = false;
         islaActual = null;
+        velocidadCaída = 1.0;
     }
 
     public void caer() {
         if (!estaEnIsla) {
-            y++;
+            y += velocidadCaída;
         }
     }
 
-    public void aterrizarEnIsla(Isla isla) {
-        if (!estaEnIsla) {
-            y = isla.getY();
-            estaEnIsla = true;
-            islaActual = isla;
-        }
-    }
 
-    public void actualizarPosicion(List<Isla> islas) {
+
+    public void actualizarPosicion(Isla[] islas) {
         caer();
+        boolean aterrizado = false;
         for (Isla isla : islas) {
             if (isla.contienePunto(x, y)) {
                 aterrizarEnIsla(isla);
-                break;
+                aterrizado = true;
             }
         }
-        if (estaEnIsla) {
+        if (estaEnIsla && aterrizado) {
             moverEnIsla();
         }
     }
+
+
+
     public boolean estaEnIsla(Isla isla) {
         return islaActual == isla;
     }
+
+
+    Tortuga[] tortugas = new Tortuga[6];
+
+
 
     public void dibujar(Entorno entorno) {
         entorno.dibujarRectangulo(this.x, this.y, this.ancho, this.alto, 0, Color.blue);
@@ -57,11 +62,7 @@ public class Tortuga {
     public double getY() {
         return this.y;
     }
-    public double getAncho() {
-        return this.ancho;
-    }
-    public double getAlto() {
-        return this.alto;
-    }
+    public double getAncho() {return this.ancho;}
+    public double getAlto() {return this.alto;}
 
 }
