896e1206040871efcc8cc54436f79c9a66681593
nicofvargas <nicofvargass@gmail.com>
2024-10-22 12:56:13 -0300

    metodos dibujar agregados

--- src/Juego/Hud.java
+++ src/Juego/Hud.java
@@ -1,5 +1,7 @@
 package Juego;
 
+import entorno.Entorno;
+
 public class Hud {
     private int cronometro;
     private int duendesSalvados;
@@ -13,8 +15,8 @@ public class Hud {
         this.enemigosEliminados=0;
     }
     //aca los metodos que actualizan cada valor
-    public void setCronometro(int milisegundos) {
-        this.cronometro=milisegundos/1000;
+    public void setCronometro(Entorno entorno) {
+        this.cronometro=entorno.tiempo()/1000;
     }
     public void setDuendesSalvados(int salvado) {
         this.duendesSalvados+=salvado;
@@ -51,5 +53,19 @@ public class Hud {
     }
     public String enemigosEliminadosStr() {
         return "Enemigos eliminados: " + getEnemigosEliminados();
+    }
+
+    //metodos para dibujar
+    public void dibujarCronometro(Entorno entorno) {
+        entorno.escribirTexto(cronometroStr(),700,25);
+    }
+    public void dibujarDuendesSalvados(Entorno entorno) {
+        entorno.escribirTexto(duendesSalvadosStr(),25,25);
+    }
+    public void dibujarDuendesMuertos(Entorno entorno) {
+        entorno.escribirTexto(duendesMuertosStr(),150,25);
+    }
+    public void dibujarEnemigosEliminados(Entorno entorno) {
+        entorno.escribirTexto(enemigosEliminadosStr(),300,25);
     }
 }
