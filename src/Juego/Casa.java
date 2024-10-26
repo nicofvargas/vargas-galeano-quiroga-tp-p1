4de57e64a51c2c59a06f13e242d7fb88d98fbc79
nicofvargas <nicofvargass@gmail.com>
2024-10-26 14:06:36 -0300

    metodos y atributos agregados

--- src/Juego/Casa.java
+++ src/Juego/Casa.java
@@ -1,4 +1,30 @@
 package Juego;
 
+import entorno.Entorno;
+
+import java.awt.*;
+
 public class Casa {
+    private double x;
+    private double y;
+    private double ancho;
+    private double alto;
+
+    public Casa(Isla[] islas) {
+        this.x=islas[0].getX();
+        this.y=islas[0].getY()-islas[0].getAlto()-this.alto;
+        this.ancho=30;
+        this.alto=30;
+    }
+
+    public void dibujar(Entorno entorno) {
+        entorno.dibujarRectangulo(this.x,this.y,this.ancho,this.alto,0, Color.white);
+    }
+
+    public double getX() {
+        return this.x;
+    }
+    public double getY() {
+        return this.y;
+    }
 }
