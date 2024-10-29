53f1208107f9824332cf03b56371c4238e5880a6
Darmus88 <juampigaleano37@gmail.com>
2024-10-28 21:20:29 -0300

    casa imagen

--- src/Juego/Casa.java
+++ src/Juego/Casa.java
@@ -1,6 +1,7 @@
 package Juego;
 
 import entorno.Entorno;
+import entorno.Herramientas;
 
 import java.awt.*;
 
@@ -9,16 +10,19 @@ public class Casa {
     private double y;
     private double ancho;
     private double alto;
+    private String rutaImagen="Images/Casa/casa.png";
+    private Image imagenCasa;
 
     public Casa(Isla[] islas) {
         this.x=islas[0].getX();
         this.y=islas[0].getY()-islas[0].getAlto()-this.alto;
-        this.ancho=30;
-        this.alto=30;
+        this.ancho=100;
+        this.alto=100;
+        this.imagenCasa= Herramientas.cargarImagen(rutaImagen).getScaledInstance((int) this.ancho,(int) this.alto, Image.SCALE_SMOOTH);
     }
 
     public void dibujar(Entorno entorno) {
-        entorno.dibujarRectangulo(this.x,this.y,this.ancho,this.alto,0, Color.white);
+        entorno.dibujarImagen(imagenCasa,this.x,this.y,0);
     }
 
     public double getX() {
